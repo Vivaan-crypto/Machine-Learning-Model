d9ec646fc1c874316d58a7e049d76aa417d04b24
Vivaan S. <72049995+Vivaan-crypto@users.noreply.github.com>
2024-01-08 22:01:38 -0500

    Adjusted Plot settings and added a sandbox file for learning while making this project

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import display
 
 # --------------------------------------------------------------
 # Load data
@@ -13,18 +14,31 @@ dataFrame = pd.read_pickle("../../data/interim/01_data_processed.pkl")
 # --------------------------------------------------------------
 
 setDataFrame = dataFrame[dataFrame["set"] == 1]
-plt.plot(setDataFrame["acc_x"])
+plt.plot(setDataFrame["acc_y"].reset_index(drop=True))
 
 # --------------------------------------------------------------
 # Plot all exercises
 # --------------------------------------------------------------
+for label in dataFrame["label"].unique():
+    subset = dataFrame[dataFrame["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+    plt.show()
 
+for label in dataFrame["label"].unique():
+    subset = dataFrame[dataFrame["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset[:100]["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+    plt.show()
 
 # --------------------------------------------------------------
 # Adjust plot settings
 # --------------------------------------------------------------
-
-
+mpl.style.use("seaborn-v0_8-deep")
+mpl.rcParams["figure.figsize"] = (20, 5)
+mpl.rcParams["figure.dpi"] = 100
 # --------------------------------------------------------------
 # Compare medium vs. heavy sets
 # --------------------------------------------------------------
