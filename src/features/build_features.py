fdd97ba660b33f6f95c2b9fefd1e0093773c9dec
Vivaan S. <72049995+Vivaan-crypto@users.noreply.github.com>
2024-02-08 21:46:10 -0500

    Done with modifying the data and added clustering and cleaned up the data a little bit. The next step is to actually start making the predictive model

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
+from FrequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 from TemporalAbstraction import NumericalAbstraction
 
 # --------------------------------------------------------------
@@ -78,33 +80,198 @@ for col in predictorColumns:
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
+dataFramePCA = dataFrameLowPass.copy()
+PCA = PrincipalComponentAnalysis()
+
+PCValues = PCA.determine_pc_explained_variance(dataFrame, predictorColumns)
+
+plt.figure(figsize=(10, 10))
+plt.plot(range(1, len(predictorColumns) + 1), PCValues)
+plt.xlabel("Principal Component Number")
+plt.ylabel("Explained Variance")
+plt.show()
+
+dataFramePCA = PCA.apply_pca(dataFramePCA, predictorColumns, 3)
+
+subset = dataFramePCA[dataFrameLowPass["set"] == 35]
+subset[
+    [
+        "pca_1",
+        "pca_2",
+        "pca_3",
+    ]
+].plot()
 
 
 # --------------------------------------------------------------
 # Sum of squares attributes
 # --------------------------------------------------------------
 
+dataFrameSquared = dataFramePCA.copy()
+
+acc_r = (
+    dataFrameSquared["acc_x"] ** 2
+    + dataFrameSquared["acc_y"] ** 2
+    + dataFrameSquared["acc_z"] ** 2
+)
+gyr_r = (
+    dataFrameSquared["gyr_x"] ** 2
+    + dataFrameSquared["gyr_y"] ** 2
+    + dataFrameSquared["gyr_z"] ** 2
+)
+dataFrameSquared["acc_r"] = np.sqrt(acc_r)
+dataFrameSquared["gyr_r"] = np.sqrt(gyr_r)
+
+subset = dataFrameSquared[dataFrameLowPass["set"] == 14]
+subset[
+    [
+        "acc_r",
+        "gyr_r",
+    ]
+].plot(subplots=True)
 
 # --------------------------------------------------------------
 # Temporal abstraction
 # --------------------------------------------------------------
 
+dataFrameTemporal = dataFrameSquared.copy()
+numAbs = NumericalAbstraction()
+
+predictorColumns += ["acc_r", "gyr_r"]
+
+windowSize = int(1000 / 200)
+
+for col in predictorColumns:
+    dataFrameTemporal = numAbs.abstract_numerical(
+        dataFrameTemporal, [col], windowSize, "mean"
+    )
+    dataFrameTemporal = numAbs.abstract_numerical(
+        dataFrameTemporal, [col], windowSize, "std"
+    )
+
 
 # --------------------------------------------------------------
 # Frequency features
 # --------------------------------------------------------------
+dataFrameFrequency = dataFrameTemporal.copy().reset_index()
 
+
+FreqAps = FourierTransformation()
+
+frameSize = int(1000 / 200)
+windowSize = int(2800 / 200)
+dataFrameFrequency = FreqAps.abstract_frequency(
+    dataFrameFrequency, ["acc_y"], windowSize, frameSize
+)
+# visualize features
+subset = dataFrameFrequency[dataFrameFrequency["set"] == 15]
+subset[["acc_y"]].plot()
+subset[
+    [
+        "acc_y_max_freq",
+        "acc_y_freq_weighted",
+        "acc_y_pse",
+        "acc_y_freq_1.429_Hz_ws_14",
+        "acc_y_freq_2.5_Hz_ws_14",
+    ]
+].plot()
+
+dataFrameFrequencyList = []
+for s in dataFrameFrequency["set"].unique():
+    print(f"Applying transformation to set {s}")
+    subset = (
+        dataFrameFrequency[dataFrameFrequency["set"] == s].reset_index(drop=True).copy()
+    )
+    subset = FreqAps.abstract_frequency(subset, predictorColumns, windowSize, frameSize)
+    dataFrameFrequencyList.append(subset)
+
+dataFrameFrequency = pd.concat(dataFrameFrequencyList).set_index(
+    "epoch (ms)", drop=True
+)
 
 # --------------------------------------------------------------
 # Dealing with overlapping windows
 # --------------------------------------------------------------
+subset = dataFrameFrequency[dataFrameFrequency["set"] == 15]
+subset[["acc_y"]].plot()
+subset[
+    [
+        "acc_x",
+        "acc_y",
+        "acc_z",
+        "gyr_x",
+        "gyr_y",
+        "gyr_z",
+    ]
+].plot()
 
+dataFrameFrequency = dataFrameFrequency.dropna()
+dataFrameFrequency = dataFrameFrequency.iloc[::2]
 
+subset = dataFrameFrequency[dataFrameFrequency["set"] == 15]
+subset[
+    [
+        "acc_x",
+        "acc_y",
+        "acc_z",
+        "gyr_x",
+        "gyr_y",
+        "gyr_z",
+    ]
+].plot()
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
+dataFrameCluster = dataFrameFrequency.copy()
 
+clusterCol = ["acc_x", "acc_y", "acc_z"]
+kVals = range(2, 10)
+inertias = []
+
+for k in kVals:
+    subset = dataFrameCluster[clusterCol]
+    kMeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    clusterLabel = kMeans.fit_predict(subset)
+    inertias.append(kMeans.inertia_)
+
+plt.figure(figsize=(10, 10))
+plt.plot(kVals, inertias)
+plt.xlabel("k")
+plt.ylabel("inertia")
+plt.show()
+
+kMeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = dataFrameCluster[clusterCol]
+dataFrameCluster["cluster"] = kMeans.fit_predict(subset)
+
+# Plotting(visualizing) the clusters
+
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for c in dataFrameCluster["cluster"].unique():
+    subset = dataFrameCluster[dataFrameCluster["cluster"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+# Plotting(visualizing) the labels
+
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for l in dataFrameCluster["label"].unique():
+    subset = dataFrameCluster[dataFrameCluster["label"] == l]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=l)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+dataFrameCluster.to_pickle("../../data/interim/03_data_features.pkl")
